f321ccf6cdff7f3eefd75fd40bb9a4182e607789
Shinta Tri Wahyuning <shintatriwahyuning@gmail.com>
2015-10-17 21:35:07 +0700

    TUGAS 3 PERULANGAN

--- Perulangan/Form1.vb
+++ Perulangan/Form1.vb
@@ -1,3 +1,24 @@
 ﻿Public Class Form1
 
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub MenuStrip2_ItemClicked(sender As Object, e As ToolStripItemClickedEventArgs) Handles MenuStrip2.ItemClicked
+
+    End Sub
+
+    Private Sub ProblemBintangToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ProblemBintangToolStripMenuItem.Click
+        Form2.MdiParent = Me
+        Form2.Show()
+    End Sub
+
+    Private Sub MatriksKalkulatorToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles MatriksKalkulatorToolStripMenuItem.Click
+        Form3.MdiParent = Me
+        Form3.Show()
+    End Sub
+
+    Private Sub KeluarToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles KeluarToolStripMenuItem.Click
+        Environment.Exit(0)
+    End Sub
 End Class
